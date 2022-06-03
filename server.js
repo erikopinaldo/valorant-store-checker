4c01eb7f97060c37d7b723e0004cf32f339c2d55
Erik Opinaldo <erikopinaldo@gmail.com>
2022-06-03 16:47:25 -0400

    updated css and js references to point to public folder

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const figlet = require('figlet')
 
 // Set rendering engine
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 app.listen(8000, () => {
   console.log('listening on 8000')
