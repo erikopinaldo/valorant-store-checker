ea8972b8843093b6f48ef36756bc00de73c5ad3a
Erik Opinaldo <erikopinaldo@gmail.com>
2022-06-03 17:19:32 -0400

    added 404 redirect

--- server.js
+++ server.js
@@ -65,3 +65,12 @@ app.get('/api/result', (req, res) => {
           console.log(error);
     }) 
 })
+
+// This route will handle all the requests that are 
+// not handled by any other route handler. In 
+// this hanlder we will redirect the user to 
+// an error page with NOT FOUND message and status
+// code as 404 (HTTP status code for NOT found)
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>404! Page not found</h1>');
+});
