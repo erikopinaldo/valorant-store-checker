7acc30d145c97b33cce8004f4c551d25c1df4f25
Erik Opinaldo <erikopinaldo@gmail.com>
2022-06-03 08:27:09 -0400

    added basic store display structure

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 document.querySelector('#clickMe').addEventListener('click', makeReq)
 
+function hideLogin() {
+    document.querySelector(".login").classList.add('hidden')
+}
+
+function showSkins() {
+    document.querySelector(".skins").classList.remove('hidden')
+}
+
 async function makeReq(){
     try {
         const res = await fetch(`/api/result`)
         const data = await res.json()
         console.log(data);
+        hideLogin();
+        showSkins();
         document.querySelector("#skin1").textContent = data.result[0]
         document.querySelector("#skin2").textContent = data.result[1]
         document.querySelector("#skin3").textContent = data.result[2]
